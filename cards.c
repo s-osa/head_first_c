ed2191a653fa9145cbbc6c2919cb020ba4e0c6d5
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2017-01-07 00:44:53 +0900

    Card count program

--- cards.c
+++ cards.c
@@ -7,27 +7,39 @@
 
 int main() {
   char card_name[3];
-  puts("カード名を入力してください：");
-  scanf("%2s", card_name);
-  int val = 0;
+  int count = 0;
 
-  switch(card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-      val = 10;
-      break;
-    case 'A':
-      val = 11;
-      break;
-    defaut:
-      val = atoi(card_name);
-  }
+  do {
+    puts("カード名を入力してください：");
+    scanf("%2s", card_name);
+    int val = 0;
 
-  if ((val >= 3) && (val <= 6))
-    puts("カウントが増えます");
-  else if (val == 10)
-    puts("カウントが減ります");
+    switch(card_name[0]) {
+      case 'K':
+      case 'Q':
+      case 'J':
+        val = 10;
+        break;
+      case 'A':
+        val = 11;
+        break;
+      case 'X':
+        continue;
+      default:
+        val = atoi(card_name);
+        if (val < 1 || val > 10) {
+          puts("入力エラーです");
+          continue;
+        }
+    }
+
+    if (val >= 3 && val <= 6)
+      count++;
+    else if (val == 10)
+      count--;
+
+    printf("現在のカウント：%i\n", count);
+  } while (card_name[0] != 'X');
 
   return 0;
 }
