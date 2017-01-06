ddb01dbd6d6f42acba28c75aca964fc015f52322
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2017-01-07 00:18:17 +0900

    Use switch

--- cards.c
+++ cards.c
@@ -11,16 +11,17 @@ int main() {
   scanf("%2s", card_name);
   int val = 0;
 
-  if (card_name[0] == 'K') {
-    val = 10;
-  } else if (card_name[0] == 'Q') {
-    val = 10;
-  } else if (card_name[0] == 'J') {
-    val = 10;
-  } else if (card_name[0] == 'A') {
-    val = 11;
-  } else {
-    val = atoi(card_name);
+  switch(card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case 'A':
+      val = 11;
+      break;
+    defaut:
+      val = atoi(card_name);
   }
 
   if ((val >= 3) && (val <= 6))
